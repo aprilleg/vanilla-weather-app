1fb522c16492614e80800c9753e5537882d3958c
aprilleg <aprilleguevarra1@gmail.com>
2023-07-14 18:41:15 +0800

    Added icon funtion

--- src/script.js
+++ src/script.js
@@ -1,42 +1,48 @@
-function formatDate(timestamp){
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10){
-        minutes = `0${minutes}`;
-    }
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let day = days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
+  return `${day} ${hours}:${minutes}`;
 }
 
-
 function displayTemperature(response) {
-    console.log(response.data);
-    let city = document.querySelector("#city");
-    let temperature = document.querySelector("#temperature");
-    let description = document.querySelector("#description");
-    let humidity = document.querySelector("#humidity");
-    let wind = document.querySelector("#wind");
-    let date = document.querySelector("#date");
+  let city = document.querySelector("#city");
+  let temperature = document.querySelector("#temperature");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let date = document.querySelector("#date");
+  let icon = document.querySelector("#icon");
 
-
-
-    city.innerHTML = response.data.name;
-    temperature.innerHTML = Math.round(response.data.main.temp);
-    description.innerHTML = response.data.weather[0].description; 
-    humidity.innerHTML = response.data.main.humidity;
-    wind.innerHTML = Math.round(response.data.wind.speed);
-    date.innerHTML = formatDate(response.data.dt *1000);
-
+  city.innerHTML = response.data.name;
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  date.innerHTML = formatDate(response.data.dt * 1000);
+  icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  icon.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "4701c6238d7532c7377eeddfdc5b837d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Philippines&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dubai&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 
